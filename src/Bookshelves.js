b7efe93ef6afa18d6e4fe02779ea914b9990e439
camilafiaminghi <camila.fiaminghi@gmail.com>
2018-10-22 20:11:15 -0300

    Add node-sass to load scss files

---
+++ src/Bookshelves.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Bookshelves.css';
+
+class Bookshelves extends Component {
+  render() {
+    return (
+      <div className="bookshelves-title">
+        <h1>MyReads</h1>
+      </div>
+    );
+  }
+}
+
+export default Bookshelves;
