ef974773b9ed1670806d13fbc1f37f894ad51112
camila_fiaminghi <camila.fiaminghi@gmail.com>
2018-10-30 08:31:38 -0300

    Refactoring component ChangeBookshelf to stateless function and show current value

--- src/ChangeBookshelf.js
+++ src/ChangeBookshelf.js
@@ -1,40 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import defaultBookshelves from './defaultBookshelves';
 import './ChangeBookshelf.scss';
 
-class ChangeBookshelf extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: 'move'
-		}
-	}
+const ChangeBookshelf = ({book, handleUpdateBookshelf}) => {
+	const shelf = (book.hasOwnProperty('shelf')) ? book.shelf : 'none';
 
-	static propTypes = {
-		book: PropTypes.object.isRequired,
-		handleUpdateBookshelf: PropTypes.func.isRequired
-	}
-
-	handleChange = (event) => {
-		this.props.handleUpdateBookshelf(this.props.book, event.target.value);
-	}
-
-	render() {
-		const shelf = (this.props.book.hasOwnProperty('shelf')) ? this.props.book.shelf : 'none';
-
-		return (
-			<div className="bookshelf-changer">
-				<select onChange={(event) => this.handleChange(event)} value={this.state.value}>
-					<option value="move" disabled="">Move to...</option>
-					{defaultBookshelves.map((bookshelf, index) => (
-						(bookshelf.value !== shelf) &&
-						(<option key={index} value={bookshelf.value}>{bookshelf.label}</option>)
+	return (
+		<div className="bookshelf-changer">
+ 				<select
+ 					value={shelf}
+ 					onChange={(event) => handleUpdateBookshelf(book, event.target.value)}>
+ 					<option value="move" disabled="">Move to...</option>
+ 					{defaultBookshelves.map((bookshelf, index) => (
+						<option
+							key={index}
+							value={bookshelf.value}>{bookshelf.label}</option>
 					))}
 				</select>
 			</div>
-		)
-	}
-}
+	);
+};
+
+ChangeBookshelf.propTypes = {
+  book: PropTypes.object.isRequired,
+  handleUpdateBookshelf: PropTypes.func.isRequired
+};
 
 export default ChangeBookshelf;
